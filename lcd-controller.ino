9633c8a2a7cd93223ccafbcb8ec0327f2a29b98c
anseonghyeon <anseonghyeon@gmail.com>
2022-12-31 14:35:56 +0900

    Update lcd-controller.ino

--- lcd-controller.ino
+++ lcd-controller.ino
@@ -7,17 +7,16 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2);
 void setup() {
 	lcd.init();
 	lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("  Seonghyeon's Desk   Seonghyeon's Desk ");
+  lcd.setCursor(0, 1);
+  lcd.print("0123456789012345678901234567890123456789");
+ 
+  delay(1000);
 }
 
 void loop() { 
-  for(int i = 0; i < 16; i++)
-  {
-    lcd.setCursor(i, 0);
-    lcd.print("Seonghyeon's");
-    lcd.setCursor(i, 1);
-    lcd.print("Desk");
-    delay(1000);
-    lcd.clear();
-  }
-
+  lcd.scrollDisplayRight();
+  delay(1000);
 }
